b5e20661733a885728e79d8fbfb1467083ed4f34
shoaibfarooq3 <shoaibfarooq3@gmail.com>
2023-05-24 03:14:44 -0700

    Console Clear

--- src/util/sendSocketNotification.js
+++ src/util/sendSocketNotification.js
@@ -3,11 +3,11 @@ var { IS_PRODUCTION } = process.env
 import { ObjectId } from 'mongodb';
 // import { find } from '../models/User.js';
 async function sendNotficationToSocket(userId, notificationId, notifierId) {
-    console.log("userId, notificationId, notifierId ", userId, notificationId, notifierId);
+    // console.log("userId, notificationId, notifierId ", userId, notificationId, notifierId);
     notifierId = notifierId.toString();
     userId = userId.toString();
     notificationId = notificationId.toString();
-    console.log("notifierId ", notifierId);
+    // console.log("notifierId ", notifierId);
     var socketUrl;
     if (IS_PRODUCTION) {
         socketUrl = 'wss://new.unitedmarket.com/'
@@ -30,11 +30,11 @@ async function sendNotficationToSocket(userId, notificationId, notifierId) {
     // });
 }
 async function sendNotficationToAll(userId, notificationId, notifierId) {
-    console.log("notification to all")
-    console.log("userId, notificationId, notifierId ", userId, notificationId, notifierId)
+    // console.log("notification to all")
+    // console.log("userId, notificationId, notifierId ", userId, notificationId, notifierId)
     userId = userId.toString()
     notificationId = notificationId.toString()
-    console.log("notifierId ", notifierId);
+    // console.log("notifierId ", notifierId);
     var socketUrl;
     if (IS_PRODUCTION) {
         socketUrl = 'wss://new.unitedmarket.com/'
