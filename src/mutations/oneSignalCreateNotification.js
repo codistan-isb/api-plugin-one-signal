34d54c398e223c28d279f79f2956af78e0937fe2
shoaibfarooq3 <shoaibfarooq3@gmail.com>
2023-07-22 20:10:05 +0500

    add:All admin ids in create Notification

--- src/mutations/oneSignalCreateNotification.js
+++ src/mutations/oneSignalCreateNotification.js
@@ -1,16 +1,21 @@
 const CUSTOMER_ONESIGNAL_APP_ID = process.env.CUSTOMER_ONESIGNAL_APP_ID;
-const CUSTOMER_ONESIGNAL_REST_API_KEY = process.env.CUSTOMER_ONESIGNAL_REST_API_KEY;
+const CUSTOMER_ONESIGNAL_REST_API_KEY =
+  process.env.CUSTOMER_ONESIGNAL_REST_API_KEY;
 const RIDER_ONESIGNAL_APP_ID = process.env.RIDER_ONESIGNAL_APP_ID;
 const RIDER_ONESIGNAL_REST_API_KEY = process.env.RIDER_ONESIGNAL_REST_API_KEY;
 const ADMIN_ONESIGNAL_APP_ID = process.env.ADMIN_ONESIGNAL_APP_ID;
 const ADMIN_ONESIGNAL_REST_API_KEY = process.env.ADMIN_ONESIGNAL_REST_API_KEY;
 import OneSignal from "onesignal-node";
 
-
-export default async function oneSignalCreateNotification(context, { message, id, appType, userId, orderID }) {
-
-  if (appType === 'rider') {
-    const riderClient = new OneSignal.Client(RIDER_ONESIGNAL_APP_ID, RIDER_ONESIGNAL_REST_API_KEY);
+export default async function oneSignalCreateNotification(
+  context,
+  { message, id, appType, userId, orderID }
+) {
+  if (appType === "rider") {
+    const riderClient = new OneSignal.Client(
+      RIDER_ONESIGNAL_APP_ID,
+      RIDER_ONESIGNAL_REST_API_KEY
+    );
     // console.log("RIDER_ONESIGNAL_APP_ID ", RIDER_ONESIGNAL_APP_ID);
     // console.log("RIDER_ONESIGNAL_REST_API_KEY ", RIDER_ONESIGNAL_REST_API_KEY)
     const notification = {
@@ -20,9 +25,9 @@ export default async function oneSignalCreateNotification(context, { message, id
       data: {
         id: id,
         type: appType,
-        orderID:""
+        orderID: "",
       },
-      include_external_user_ids: [userId]
+      include_external_user_ids: [userId],
     };
     // console.log("notification obj for riderClient: ", notification);
 
@@ -32,12 +37,14 @@ export default async function oneSignalCreateNotification(context, { message, id
       return {
         statusCode: 200,
         msg: "Notification created",
-      }
+      };
     }
-
   }
-  if (appType === 'customer') {
-    const customerClient = new OneSignal.Client(CUSTOMER_ONESIGNAL_APP_ID, CUSTOMER_ONESIGNAL_REST_API_KEY);
+  if (appType === "customer") {
+    const customerClient = new OneSignal.Client(
+      CUSTOMER_ONESIGNAL_APP_ID,
+      CUSTOMER_ONESIGNAL_REST_API_KEY
+    );
 
     // console.log("customerClient ", customerClient)
     const notification = {
@@ -47,25 +54,33 @@ export default async function oneSignalCreateNotification(context, { message, id
       data: {
         id: id,
         type: appType,
-        orderID: orderID
+        orderID: orderID,
       },
-      include_external_user_ids: [userId]
+      include_player_ids: ["4e997099-ff82-49ce-b45c-0ef5228e657b"],
+      // include_external_user_ids: ["4e997099-ff82-49ce-b45c-0ef5228e657b"]
     };
     // console.log("notification obj for customer Client: ", notification);
 
     const response = await customerClient.createNotification(notification);
-    // console.log("customer Client response ", response)
+    console.log("customer Client response ", response);
     if (response.statusCode === 200) {
       return {
         statusCode: 200,
         msg: "Notification created",
-      }
+      };
     }
-
-
   }
-  if (appType === 'admin') {
-    const adminClient = new OneSignal.Client(ADMIN_ONESIGNAL_APP_ID, ADMIN_ONESIGNAL_REST_API_KEY);
+  if (appType === "admin") {
+    const { Accounts } = context.collections;
+    const allAdminUsers = await Accounts.find({ UserRole: "admin" }).toArray();
+    // console.log("allAdminUsers ", allAdminUsers[0]._id);
+    let idsArray = allAdminUsers.map((user) => user._id);
+    // console.log("allAdminUsers ", idsArray);
+
+    const adminClient = new OneSignal.Client(
+      ADMIN_ONESIGNAL_APP_ID,
+      ADMIN_ONESIGNAL_REST_API_KEY
+    );
     // console.log("admin App id ", ADMIN_ONESIGNAL_APP_ID)
     // console.log("admin API key ", ADMIN_ONESIGNAL_REST_API_KEY)
     const notification = {
@@ -75,21 +90,20 @@ export default async function oneSignalCreateNotification(context, { message, id
       data: {
         id: id,
         type: appType,
-        orderID:""
+        orderID: "",
       },
-      include_external_user_ids: [userId]
+      // include_player_ids: ["4e997099-ff82-49ce-b45c-0ef5228e657b"],
+      include_external_user_ids: idsArray,
     };
-    // console.log("notification obj for admin Client: ", notification);
+    console.log("notification obj for admin Client: ", notification);
 
     const response = await adminClient.createNotification(notification);
-    // console.log(" Admin response ", response)
+    console.log(" Admin response ", response);
     if (response.statusCode === 200) {
       return {
         statusCode: 200,
         msg: "Notification created",
-      }
+      };
     }
-
-
   }
 }
